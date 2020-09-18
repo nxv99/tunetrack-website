24b9c372a550efd48dad62697813ec43b7ed7f25
audn <audunhilden@live.no>
2020-09-18 20:45:45 +0200

    fix

--- layouts/default.js
+++ layouts/default.js
@@ -3,7 +3,7 @@ import Footer from '../components/footer';
 
 function Layout(props) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className=" flex-col min-h-screen">
       <Header />
       {props.children}
       <Footer />
